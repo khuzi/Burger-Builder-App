aacb18e5b64133e4c106c76c1de597f0d36a329c
Muhammad Khuzaima <55664788+khuzi@users.noreply.github.com>
2020-07-11 23:28:50 +0500

    Adding some more files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component }  from 'react';
+import { Route , Switch , withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux'; 
+import * as actions from './store/actions/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Layout from './hoc/Layout/Layout.component'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder.component'
+import Checkout from '../src/containers/Checkout/Checkout.component';
+import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.onAutoTrySignUp();
+  }
+  render() { 
+    return ( 
+      <div>
+        <Layout />
+        <Switch> 
+          <Route path="/checkout" component={Checkout}/> 
+          <Route path="/orders"  component={Orders}/>  
+          <Route path="/auth"  component={Auth}/>
+          <Route path="/logout"  component={Logout}/>    
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Redirect to="/"/>
+        </Switch>
+      </div>
+     ); 
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onAutoTrySignUp: () => dispatch(actions.authCheckState())
+  }
+}
+
+
+export default withRouter(connect(null ,mapDispatchToProps)(App));
+
+
+
+
+//with exact the order wouldn't matter but without exact the order is matter.
--- src/index.js
+++ src/index.js
@@ -3,10 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore , applyMiddleware , compose , combineReducers } from 'redux'; 
+import thunk from 'redux-thunk';
 
-ReactDOM.render(
+import BurgerBuilderReducer from './store/reducers/BurgerBuilder';
+import OrderReducer from './store/reducers/order';
+import AuthReducer from './store/reducers/auth';
+
+const rootReducer = combineReducers({
+  burgerBuilder: BurgerBuilderReducer,
+  order: OrderReducer,
+  auth: AuthReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render( 
   <React.StrictMode>
-    <App />
+    {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
